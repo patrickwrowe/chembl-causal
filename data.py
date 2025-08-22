45603bf545c92b26450a188da425c33cf2a6b681
Patrick Rowe <patrick.william.rowe@gmail.com>
2025-08-22 16:26:42 -0230

    Add example of FCI algorithm for inferring causal relations in physicochemical assays for larger/complex dataset.

--- data.py
+++ data.py
@@ -13,6 +13,16 @@ CHEMBL_ASSAY_DESCRIPTION_REMAPPING = {
     'value_Dissociation constant (pKa)': 'pKa',
     'value_Dissociation constant, pKa of the compound': 'pKa',
     'value_Ionization constant (pKa)': 'pKa',
+
+    # Rare pKA assays
+    'value_ASTRAZENECA: Most acidic pKa value (pKa A1) determined by absorption and potentiometric titration using standard methodology from Sirius Analytical. Experimental range Acids: <= 11.': 'pKa',
+    'value_Dissociation constant, pKa of compound': 'pKa',
+    'value_Ionisation constant (pKa)': 'pKa',
+    'value_pKa (acid-base dissociation constant) as determined by Avdeef ref: DOI: 10.1002/047145026X': 'pKa',
+    'value_pKa (acid-base dissociation constant) as determined by Liao ref: J Chem Info Model 2009': 'pKa',
+    'value_pKa (acid-base dissociation constant) as determined by Morgenthaler ref: ChemMedChem 2007': 'pKa',
+    'value_pKa (acid-base dissociation constant) as determined by potentiometric titration': 'pKa',
+    'value_pKa value was determined': 'pKa',
     
     # LogD (distribution coefficient at pH 7.4)
     'value_ASTRAZENECA: Octan-1-ol/water (pH7.4) distribution coefficent measured  by a shake flask method described in J. Biomol. Screen. 2011, 16, 348-355. Experimental range -1.5 to 4.5': 'LogD',
@@ -30,6 +40,28 @@ CHEMBL_ASSAY_DESCRIPTION_REMAPPING = {
     'value_Partition coefficient (logD7.4)': 'LogD',
     'value_SUPPLEMENTARY: Lipophilicity (log D) Determination by high-throughput shake-flask': 'LogD',
     
+    # rare logD assays at pH7.4
+    'value_DNDI: Lipophilicity (gLogD) at pH 7.4': 'LogD',
+    'value_Distribution coefficient, log D at pH 7.4': 'LogD',
+    'value_Distribution coefficient, logD of compound at pH 7.4': 'LogD',
+    'value_GSK_TB: GSK in-house hydrophobicity assay at pH7.4 (logD)': 'LogD',
+    'value_Lipophilicity, log D of compound': 'LogD',
+    'value_Lipophilicity, log D of the compound by HPLC analysis': 'LogD',
+    'value_Lipophilicity, logD of compound': 'LogD',
+    'value_Lipophilicity, logD of compound at pH 7.4 by shake flask method': 'LogD',
+    'value_Lipophilicity, logD of the compound': 'LogD',
+    'value_Lipophilicity, logD of the compound at pH 7.4 by shake flask method': 'LogD',
+    'value_Partition coefficient (logD)': 'LogD',
+    'value_Partition coefficient, log D at pH 7.4': 'LogD',
+    'value_Partition coefficient, log D of the compound at pH 7.4': 'LogD',
+    'value_Partition coefficient, log D of the compound at pH 7.4 by HPLC analysis': 'LogD',
+    # Specialized lipophilicity measures
+    'value_Lipophilicity determined as logarithm of the partition coefficient in the alkane/water system': 'LogP_alkane',
+    'value_Lipophilicity, log K of the compound': 'LogK',
+    # Additional pH-specific LogD
+    'value_DNDI: Lipophilicity (gLogD) at pH 3': 'LogD pH3',
+    'value_Partition coefficient (logD7.6)': 'LogD pH7.6',
+    
     # LogP (partition coefficient)
     'value_Calculated partition coefficient (clogP)': 'cLogP',
     'value_Lipophilicity, log P of the compound': 'LogP',
@@ -39,6 +71,12 @@ CHEMBL_ASSAY_DESCRIPTION_REMAPPING = {
     'value_Partition coefficient (logP)': 'LogP',
     'value_Partition coefficient (logP) (HPLC)': 'LogP',
     'value_Partition coefficient, log P of the compound': 'LogP',
+
+    # uncommon logP names/aliases
+    'value_GSK_TB: GSK in-house hydrophobicity assay (logP)': 'LogP',
+    'value_Hydrophobicity, log P of the compound in octanol-water by shaking-flask method': 'LogP',
+    'value_Lipophilicity, log P of the compound by HPLC analysis': 'LogP',
+    'value_Lipophilicity, logP of compound': 'LogP',
     
     # Aqueous solubility (pH 7.4)
     'value_ASTRAZENECA: Solubility in pH7.4 buffer using solid starting material using the method described in J. Assoc. Lab. Autom. 2011, 16, 276-284. Experimental range 0.10 to 1500 uM': 'Aqueous Solubility',
@@ -54,30 +92,77 @@ CHEMBL_ASSAY_DESCRIPTION_REMAPPING = {
     'value_Solubility of the compound in water': 'Aqueous Solubility',
     'value_Solubility of the compound at pH 7.4': 'Aqueous Solubility',
     
+    # Additional aqueous solubility, less common naming
+    'value_Aqueous solubility of compound at pH 7.4': 'Aqueous Solubility',
+    'value_Aqueous solubility of compound at pH 7.4 by HPLC analysis': 'Aqueous Solubility',
+    'value_Intrinsic solubility of the compound in water': 'Aqueous Solubility',
+    'value_Solubility (buffer pH 7.4)': 'Aqueous Solubility',
+    'value_Solubility in sodium phosphate buffer at pH 7.4': 'Aqueous Solubility',
+    'value_Solubility of compound': 'Aqueous Solubility',
+    'value_Solubility of compound at pH 7.4': 'Aqueous Solubility',
+    'value_Solubility of the compound in phosphate buffer at pH 7.4': 'Aqueous Solubility',
+    'value_Thermodynamic aqueous solubility of the compound': 'Aqueous Solubility',
+    'value_Thermodynamic solubility of the compound at pH 7.4': 'Aqueous Solubility',
+
+    # Additional pH-specific solubility
+    'value_Aqueous solubility of the compound at pH 2': 'Solubility pH2',
+    'value_Aqueous solubility of the compound at pH 6.5': 'Solubility pH6.5',
+    'value_Aqueous solubility of the compound at pH 6.8': 'Solubility pH6.8',
+    'value_Aqueous solubility of the compound at pH 7': 'Solubility pH7',
+    'value_DNDI: Solubility in 0.01M HCl at pH 2 determined by nephelometry': 'Solubility pH2',
+    'value_DNDI: Solubility in phosphate buffer at pH 6.5 determined by nephelometry': 'Solubility pH6.5',
+    'value_Kinetic solubility of compound at pH 2': 'Solubility pH2',
+    'value_Kinetic solubility of the compound at pH 6.5': 'Solubility pH6.5',
+    'value_Solubility in 100 mM bistrispropane buffer at pH 6.0 incubated for 18 hrs by UPLC analysis': 'Solubility pH6.0',
+    'value_Solubility in 100 mM bistrispropane buffer at pH 6.5 incubated for 18 hrs by UPLC analysis': 'Solubility pH6.5',
+    'value_Solubility in 100 mM bistrispropane buffer at pH 7.0 incubated for 18 hrs by UPLC analysis': 'Solubility pH7.0',
+    'value_Solubility in 100 mM bistrispropane buffer at pH 7.5 incubated for 18 hrs by UPLC analysis': 'Solubility pH7.5',
+    'value_Solubility in 100 mM bistrispropane buffer at pH 8.0 incubated for 18 hrs by UPLC analysis': 'Solubility pH8.0',
+    'value_Solubility in 100 mM glycylglycine buffer at pH 3.0 incubated for 18 hrs by UPLC analysis': 'Solubility pH3.0',
+    'value_Solubility in 100 mM glycylglycine buffer at pH 4.0 incubated for 18 hrs by UPLC analysis': 'Solubility pH4.0',
+    'value_Solubility in 100 mM glycylglycine buffer at pH 5.0 incubated for 18 hrs by UPLC analysis': 'Solubility pH5.0',
+    'value_Solubility of the compound at pH 2': 'Solubility pH2',
+    'value_Solubility of the compound in water at pH 7.0 by HPLC': 'Solubility pH7.0',
+    'value_Thermodynamic solubility of the compound at pH 6.8': 'Solubility pH6.8',
+    
     # Kinetic solubility
     'value_Kinetic aqueous solubility of the compound': 'Kinetic Solubility',
     'value_Kinetic solubility of compound': 'Kinetic Solubility',
     'value_Kinetic solubility of the compound': 'Kinetic Solubility',
     'value_Kinetic solubility of the compound at pH 7.4': 'Kinetic Solubility',
+    'value_Kinetic solubility of compound at pH 7.4': 'Kinetic Solubility',
     
     # HPLC retention time
     'value_Retention time of the compound': 'Retention Time',
     'value_Retention time of the compound by HPLC analysis': 'Retention Time',
+    'value_Retention time of the compound by HPLC method': 'Retention Time',
     
     # Special pH conditions
-    'value_Partition coefficient (logD6.5)': 'LogD_pH6.5',
-    'value_Solubility of the compound at pH 6.8': 'Solubility_pH6.8',
-    'value_Solubility of the compound at pH 7': 'Solubility_pH7',
+    'value_Partition coefficient (logD6.5)': 'LogD pH6.5',
+    'value_Solubility of the compound at pH 6.8': 'Solubility pH6.8',
+    'value_Solubility of the compound at pH 7': 'Solubility pH7',
     
-    # DMSO solubility
+
+    # Other solvents
+    'value_Solubility of the compound in ethanol': 'Ethanol Solubility',
     'value_Solubility of the compound in DMSO': 'DMSO Solubility',
     
     # Specialized assays
+    # LYSA
     'value_SUPPLEMENTARY: Lyophilisation Solubility Assay (LYSA)': 'LYSA',
-    'value_SUPPLEMENTARY: PAMPA permeability assay': 'PAMPA Permeability'
+    
+    # PAMPA (EH PAMPA, WAT DESE EARTHERS SAYIN'?)
+    'value_SUPPLEMENTARY: PAMPA permeability assay': 'PAMPA Permeability',
+    'value_Apparent permeability of the compound at 50 uM after 5 hrs by PAMPA method': 'PAMPA Permeability',
+    'value_Apparent permeability of the compound by PAMPA': 'PAMPA Permeability',
+    'value_Permeability of the compound by PAMPA': 'PAMPA Permeability',
 }
 
 def chembl_physicochemical_assays(min_raw_assay_measurements: int = 500, min_measurements_per_mol: int = 2, atol: float = 1e-9, verbose: bool = False):
+
+    if min_raw_assay_measurements < 100:
+        print("WARNING: min_raw_assay_measurements < 100. Name remapping for assays only performed up to this threshold." \
+                f"You'll start getting lots of unnormalized assay names with a threshold of {min_raw_assay_measurements}")
 
     SQL_QUERY_WITH_IDS = """SELECT 
                         molregno, assays.assay_id, description, confidence_score, chembl_id, data_validity_comment, value, units, type
@@ -121,7 +206,7 @@ def chembl_physicochemical_assays(min_raw_assay_measurements: int = 500, min_mea
     if verbose:
         print(f"Began with {len(chembl_a_p_raw)} rows, for {len(set(chembl_a_p_raw.description.to_list()))} assays and {len(set(chembl_a_p_raw.chembl_id.to_list()))} molecules.\n")
         print(f"Filtered to {len(chembl_a_p)} rows, for {len(set(chembl_a_p.description.to_list()))} assays and {len(set(chembl_a_p.chembl_id.to_list()))} moleucles.\n")
+        print(f"Name remapping brings these numbers to {len(pivoted)} rows, for {len(set(pivoted.columns.to_list())) - 1} assays.\n")
         print(f"{chembl_a_p["description"].value_counts()}")
-
 
     return pivoted
